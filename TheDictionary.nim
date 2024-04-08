1a573799da50945b6fc8653771f45de6994ff9ba
nirokay <80123634+nirokay@users.noreply.github.com>
2024-04-08 11:03:16 +0200

    removed unused module

--- TheDictionary.nim
+++
@@ -1,9 +0,0 @@
-## TheDictionary
-## =============
-##
-## TheDictionary is an [Urban Dictionary](https://www.urbandictionary.com/) webserver clone.
-##
-## All definitions are saved using SQLite.
-
-import src/dictionaryserver
-export dictionaryserver
